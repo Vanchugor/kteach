cf53062ca9ce8c70a21c73ce9b0aa9634c731eab
Eugene Kachmar <mironkacmar@mail.ru>
2023-01-01 15:50:25 +0300

    add empty backend part

---
+++ Back/ct/ct/src/main/java/com/physics30/ct/PhisicsHelperApplication.java
@@ -0,0 +1,13 @@
+package com.physics30.ct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhisicsHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhisicsHelperApplication.class, args);
+	}
+
+}
